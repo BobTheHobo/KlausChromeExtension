879e1941e11198f139a5c5e8f999136c9b922430
Viet Ngomai <thienvietngomai@gmail.com>
2023-08-24 17:22:06 -0400

    homepage add clock, navbar, message, and css

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -25,5 +25,13 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 }
